d9d25372af7c3f9e67df4ddde78d8a1bf91d6d11
MXMRD <mxmrnr@gmail.com>
2018-09-19 13:29:41 +0200

    Added shelf change of Books and removed code.
    Books can be moved between shelfs now. They show the correct current shelf in the selector. Old, static mark-up was removed.

--- src/Book.js
+++ src/Book.js
@@ -1,24 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 function Book (props) {
-  console.log(props)
-  
   return (
     <div className="book">
       <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
-            <select value={props.shelf}> {/*onChange={props.moveBook}*/}
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
+            <select value={props.book.shelf} onChange={(event) => props.move({id: props.book.id}, event.target.value)}>
+              <optgroup label='Move'>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option id='TAtt' value="none">None</option>
+              </optgroup>
             </select>
           </div>
       </div>
       <div className="book-title">{props.book.title}"</div>
-      <div className="book-authors">{props.book.authors}</div> {/*Change list of authors to mapped array*/}
+      {props.book.authors.map((author, key) => (
+        <div key={key} className="book-authors">{author}</div>
+      ))}
     </div>
   )
   
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Book from "./Book.js";
 
 function Shelf (props) {
-  console.log(props.books);
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{props.title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {props.books.map((BookObj, key) => (
-            <li key={key}><Book book={BookObj}/></li>
+            <li key={key}><Book book={BookObj} move={props.move}/></li>
           ))}
         </ol>
       </div>
